3c77956cbe179a35389dea07b17e52dc43d9d947
Carlos Caballero <carlos.caballero.gonzalez@gmail.com>
2023-05-26 23:18:12 +0200

    05.02-routerparams

--- src/app/pages/hero/hero-detail/hero-detail.component.ts
+++ src/app/pages/hero/hero-detail/hero-detail.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+
+import { ActivatedRoute } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { HeroItemComponent } from 'src/app/components/hero-item/hero-item.component';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-hero-detail',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HeroItemComponent],
   templateUrl: './hero-detail.component.html',
   styleUrls: ['./hero-detail.component.scss']
 })
 export class HeroDetailComponent {
+  private readonly activatedRoute = inject(ActivatedRoute);
+
+  public hero$ = this.activatedRoute.data.pipe(map(({ hero }) => hero));
 
 }
