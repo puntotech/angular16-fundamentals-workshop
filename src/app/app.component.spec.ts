310e798c7bb5e4a4192e627d7351c4bfb0cc3f78
Carlos Caballero <carlos.caballero.gonzalez@gmail.com>
2023-06-09 12:23:01 +0200

    initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -12,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'workshop-fundamentals' title`, () => {
+  it(`should have the 'workshop-angular-fundamentals' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('workshop-fundamentals');
+    expect(app.title).toEqual('workshop-angular-fundamentals');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('workshop-fundamentals app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('workshop-angular-fundamentals app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,14 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { HeroItemComponent } from './components/hero-item/hero-item.component';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeroItemComponent],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'workshop-fundamentals';
+  title = 'workshop-angular-fundamentals';
 }
